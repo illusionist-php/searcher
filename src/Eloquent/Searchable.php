ea7d6ccd7551b07173cd7751c4c8c875be6b73d3
kaysonwu <772663139@qq.com>
2022-02-23 18:09:09 +0800

    test: add debugcode

--- src/Eloquent/Searchable.php
+++ src/Eloquent/Searchable.php
@@ -235,6 +235,10 @@ trait Searchable
      */
     public function isDateAttribute($key)
     {
+        if ($key === 'created_at') {
+            var_dump(parent::isDateAttribute($key));
+        }
+
         return in_array($key, $this->getDates()) ||
             $this->isDateCastable($key);
     }
